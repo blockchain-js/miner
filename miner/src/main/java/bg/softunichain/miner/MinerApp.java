10794d5978ef1b51ee012bb2c65ef70287881d19
Hristo Tzigarov <tzigarov@gmail.com>
2018-02-26 17:25:23 +0100

    default config added

--- miner/src/main/java/bg/softunichain/miner/MinerApp.java
+++ miner/src/main/java/bg/softunichain/miner/MinerApp.java
@@ -1,9 +1,17 @@
 package bg.softunichain.miner;
 
+import bg.softunichain.miner.models.RequestBlock;
+
 public class MinerApp 
 {
+	private final static String DEFAULT_MINER_ADDRESS = "0xB282E73A4A6da70200E328e2d5427b15EEFC20b3";
+	private final static String DEFAULT_NODE_BASEURL = "http://localhost:5555";
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	MinerService ms = new MinerService(DEFAULT_NODE_BASEURL, DEFAULT_MINER_ADDRESS);
+
+		RequestBlock nextBlock = ms.getNextBlock();
+		ms.mineBlockHash(nextBlock);
     }
 }
