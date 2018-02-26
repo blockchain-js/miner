67b63706aeb1cd99691374dd52cf8264acbf3246
Hristo Tzigarov <tzigarov@gmail.com>
2018-02-26 13:09:23 +0100

    new maven project

---
+++ miner/src/main/java/bg/softunichain/miner/App.java
@@ -0,0 +1,13 @@
+package bg.softunichain.miner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
